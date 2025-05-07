1023dadb5f9164f2029c2f10f2bc0192c37dbcdc
dylee <2do00531@naver.com>
2025-05-07 10:13:36 +0900

    length

--- string/similarity.cpp
+++ string/similarity.cpp
@@ -2,6 +2,35 @@
 #include <vector>
 
 class StringSimilarity {
+	const int MAX_SCORE = 60;
 public:
+	int calculateSimilarity(const std::string& str1, const std::string& str2) {
+		int similarity = calculatekLengthSimilarity(str1, str2);
+		return similarity;
+	}
+	int calculatekLengthSimilarity(const std::string& str1, const std::string& str2) {
+		int longLength = std::max(str1.length(), str2.length());
+		int shortLength = std::min(str1.length(), str2.length());
+		if (isZeroLength (shortLength)) return 0;
+		if (isSameLength(longLength,shortLength))return MAX_SCORE;
+		if (isMoreDoblueLenth(longLength, shortLength))return 0;
+		return calculatePartialSimilarity(longLength, shortLength);
+	}
+private :
+	int calculatePartialSimilarity(int longLength, int shortLength) {
+		double cap = longLength - shortLength;
+		return (1 - cap / shortLength) * MAX_SCORE;
+	}
 
+	bool isSameLength(int longLength, int shortLength) {
+		return (longLength == shortLength);
+	}
+
+	bool isMoreDoblueLenth(int longLength, int shortLength) {
+		return (longLength >= shortLength * 2);
+	}
+
+	bool isZeroLength(int lenght) {
+		return (lenght == 0);
+	}
 };
