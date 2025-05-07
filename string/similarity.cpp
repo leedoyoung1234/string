ac068525d4027f35544035405058419a55fe84b9
dylee <2do00531@naver.com>
2025-05-07 10:43:08 +0900

    change request modification

--- string/similarity.cpp
+++ string/similarity.cpp
@@ -2,21 +2,24 @@
 #include <vector>
 
 class StringSimilarity {
-	const int MAX_SCORE = 60;
 public:
 	int calculateSimilarity(const std::string& str1, const std::string& str2) {
-		int similarity = calculatekLengthSimilarity(str1, str2);
+		int similarity = calculateLengthSimilarity(str1, str2);
 		return similarity;
 	}
-	int calculatekLengthSimilarity(const std::string& str1, const std::string& str2) {
+
+private :
+	const int MAX_SCORE = 60;
+
+	int calculateLengthSimilarity(const std::string& str1, const std::string& str2) {
 		int longLength = std::max(str1.length(), str2.length());
 		int shortLength = std::min(str1.length(), str2.length());
-		if (isZeroLength (shortLength)) return 0;
-		if (isSameLength(longLength,shortLength))return MAX_SCORE;
+		if (isZeroLength(shortLength)) return 0;
+		if (isSameLength(longLength, shortLength))return MAX_SCORE;
 		if (isMoreDoblueLenth(longLength, shortLength))return 0;
 		return calculatePartialSimilarity(longLength, shortLength);
 	}
-private :
+
 	int calculatePartialSimilarity(int longLength, int shortLength) {
 		double cap = longLength - shortLength;
 		return (1 - cap / shortLength) * MAX_SCORE;
@@ -30,7 +33,7 @@ private :
 		return (longLength >= shortLength * 2);
 	}
 
-	bool isZeroLength(int lenght) {
-		return (lenght == 0);
+	bool isZeroLength(int lengh) {
+		return (lengh == 0);
 	}
 };
